bcb74360972adb60c1e531d55a034eb20dc66ce6
Richelle Tan <r.kyj89@gmail.com>
2022-01-12 01:34:08 +0800

    Admin Registration Works

--- OAiES/urls.py
+++ OAiES/urls.py
@@ -20,17 +20,17 @@ from main.views import login_view
 from main.views import redir_view
 from main.views import userProf_view
 from main.views import dashboard_view
-from registerAdmin.views import regisAdm_view
+from registerAdmin.views import register
 from registerUser.views import regisUsr_view
 
 urlpatterns = [
     path('', landing_view, name='landing'),
-    path('landing/', landing_view),
-    path('login/', login_view),
-    path('userProf/', userProf_view),
-    path('dashboard/', dashboard_view),
+    path('landing/', landing_view, name="landingP"),
+    path('login/', login_view, name="login"),
+    path('userProf/', userProf_view, name="userProf"),
+    path('dashboard/', dashboard_view, name="Dash"),
     path('redir/', redir_view), #REMOVE LATER
-    path('register/', regisAdm_view),
-    path('registerUser/', regisUsr_view),
+    path('register/', register, name="Register"),
+    path('registerUser/', regisUsr_view, name="RegisterUser"),
     path('admin/', admin.site.urls),
 ]
--- registerAdmin/views.py
+++ registerAdmin/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import  render, redirect
+from registerAdmin.forms import RegistrationForm
 # Create your views here.
-def regisAdm_view(request, *args, **kwargs):
-    return render(request, "registerAdmin.html", {})
+
+# def regisAdm_view(request, *args, **kwargs):
+#     return render(request, "registerAdmin.html", {})
+def register(request):
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/userProf/')
+    else:
+        form = RegistrationForm()
+
+        args = {'form': form}
+        return render(request, 'registerAdmin.html', args)
