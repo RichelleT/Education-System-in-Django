bb179469a4a9e9b1aee627538245dc6e3a502ead
Richelle Tan <r.kyj89@gmail.com>
2022-01-11 20:17:09 +0800

    adjust url routing

--- OAiES/urls.py
+++ OAiES/urls.py
@@ -23,7 +23,7 @@ from registerAdmin.views import regisAdm_view
 from registerUser.views import regisUsr_view
 
 urlpatterns = [
-    # path('', landing_view, name='landing'),
+    path('', landing_view, name='landing'),
     path('landing/', landing_view),
     path('login/', login_view),
     path('userProf/', userProf_view),
