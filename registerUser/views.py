8ccfca492e714cffe6225b812d291d94c1e05306
Richelle Tan <r.kyj89@gmail.com>
2022-02-07 21:26:04 +0800

    adjust

--- registerUser/views.py
+++ registerUser/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from registerUser.forms import addUserForm
 
 def regisUsr_view(request, *args, **kwargs):
     return render(request, "registerUser.html", {})
