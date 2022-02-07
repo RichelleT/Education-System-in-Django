8fa0d2d68c5fc5aff298a44a54974eb039ef9b43
Richelle Tan <r.kyj89@gmail.com>
2022-02-07 21:16:22 +0800

    adjust navbar

--- registerUser/views.py
+++ registerUser/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 #from registerUser.forms import addUserForm
 
-# Create your views here.
 def regisUsr_view(request, *args, **kwargs):
     return render(request, "registerUser.html", {})
