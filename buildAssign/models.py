91dbfde54230e65da1a6890d81597973101060b6
Richelle Tan <r.kyj89@gmail.com>
2022-02-10 05:12:59 +0800

    set on_delete to CASCADE at models.py

--- buildAssign/models.py
+++ buildAssign/models.py
@@ -3,20 +3,20 @@ from testBuilder.models import Module
 from django.contrib.auth.models import User
 
 class Assignment(models.Model):
-    linked_module = models.ForeignKey(Module, on_delete=models.SET_NULL, null=True)
+    linked_module = models.ForeignKey(Module, on_delete=models.CASCADE, null=True)
     question = models.CharField(max_length=1000, default="")
     #must test if not working, remove answer field.
     answer = models.CharField(max_length=5000, default="")
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.question
 
 class AssignResult(models.Model):
-    linked_assign = models.ForeignKey(Assignment, on_delete=models.SET_NULL, null=True) #linking result to test paper
-    linked_module = models.ForeignKey(Module, on_delete=models.SET_NULL, null=True) #linking result to quiz questions model
-    attempted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    linked_assign = models.ForeignKey(Assignment, on_delete=models.CASCADE, null=True) #linking result to test paper
+    linked_module = models.ForeignKey(Module, on_delete=models.CASCADE, null=True) #linking result to quiz questions model
+    attempted_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     correct = models.IntegerField(default=0)
     #wrong = models.IntegerField(default=0)
     percentage = models.IntegerField(default=0)
