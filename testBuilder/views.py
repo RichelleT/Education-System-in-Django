f74e59c17521f8d6cc51d33fe33f408da85aeefb
Richelle Tan <r.kyj89@gmail.com>
2022-02-10 17:51:58 +0800

    view individual result done

--- testBuilder/views.py
+++ testBuilder/views.py
@@ -182,7 +182,7 @@ def resultPg(request, pk):
     resList = quizResult.objects.filter(linked_module=pk)
     module_title = Module.objects.get(pk=pk)
     test_title = Test.objects.get(pk=pk)
-    stuResList = quizResult.objects.filter(attempted_by=pk)
+    stuResList = quizResult.objects.filter(attempted_by=request.user, linked_module=pk)
 
     context = {
         'resList':resList,
@@ -192,12 +192,12 @@ def resultPg(request, pk):
     }
     return render(request, "viewResultsPage.html", context)
 
-def studentResult(request, pk):
-    resList = quizResult.objects.filter(attempted_by=pk)
-    module_title = Module.objects.get(pk=pk)
-    test_title = Test.objects.get(pk=pk)
+# def studentResult(request, pk):
+#     resList = quizResult.objects.filter(attempted_by=pk)
+#     module_title = Module.objects.get(pk=pk)
+#     test_title = Test.objects.get(pk=pk)
 
-    context = {
-        'resList':resList,
-    }
-    return render(request, "studentResult.html", context)
+#     context = {
+#         'resList':resList,
+#     }
+#     return render(request, "studentResult.html", context)
