ff9a4b9a9849245562eb1ee567cd8330cf19e37f
Richelle Tan <r.kyj89@gmail.com>
2022-02-11 23:28:17 +0800

    cleans models.py

--- buildAssign/models.py
+++ buildAssign/models.py
@@ -4,18 +4,10 @@ from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 
 class Assignment(models.Model):
-    # LIMIT_CHOICES = (
-    #     ('1', '1'),
-    #     ('0', '0'),
-    # )
-
     linked_module = models.ForeignKey(Module, on_delete=models.CASCADE, null=True)
     assign_name = models.CharField(max_length=1000, default="")
-    #must test if not working, remove answer field.
-    #answer = models.CharField(max_length=5000, default="")
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_date = models.DateTimeField()
-    #limit = models.CharField(max_length=1, choices=LIMIT_CHOICES, blank=False, default='1')
 
     def __str__(self):
         return self.assign_name
@@ -26,7 +18,6 @@ class Answer(models.Model):
     question = models.TextField(max_length=5000, default="")
     answer = models.TextField(max_length=5000, default="")
     created_date = models.DateTimeField()
-    #upload_txt = models.FileField(null=True, blank=True, validators=[FileExtensionValidator( ['txt'] ) ])
 
     def __str__(self):
         return self.question
@@ -44,15 +35,12 @@ class File(models.Model):
 class AssignResult(models.Model):
     link_ques = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True) 
     linked_assign = models.ForeignKey(Assignment, on_delete=models.CASCADE, null=True)
-    #linked_module = models.ForeignKey(Module, on_delete=models.CASCADE, null=True) 
     attempted_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     correct = models.IntegerField(default=0)
-    #wrong = models.IntegerField(default=0)
     percentage = models.IntegerField(default=0)
     total = models.IntegerField(default=0)
     grade = models.CharField(max_length=10, default="")
     lettergrade = models.CharField(max_length=10, default="")
-    #a_time = models.DateTimeField(auto_now=True)
     attempted_time = models.DateTimeField()
 
     def __str__(self):
