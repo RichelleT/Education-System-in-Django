4890876f1836ec7547bad8bdc4db33123ea769d6
Richelle Tan <r.kyj89@gmail.com>
2022-02-17 20:24:42 +0800

    adj

--- OAiES/settings.py
+++ OAiES/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'OAiES.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'OAiES2',
+        'NAME': 'OAiES',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
